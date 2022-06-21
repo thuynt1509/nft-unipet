aa2c490b344e8b9490eff753ed84997aab817220
thuynt <thuthuy.nguyen1509@gmail.com>
2022-06-21 22:00:25 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
-    // $(".tab-sex .btn-male").click(function () {
-    //     $(".tab-sex a").removeClass("active");
-
-    // });
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        autoplay: false,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        // centerMode: true,
+        autoplay: false,
+        loop: false,
+        focusOnSelect: true
+      });
+      
 
 });
