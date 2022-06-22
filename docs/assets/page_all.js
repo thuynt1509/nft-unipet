be7b34904f6468ff342262de602ccd48724fe134
thuynt <thuthuy.nguyen1509@gmail.com>
2022-06-23 00:36:52 +0700

    update css

--- docs/assets/page_all.js
+++ docs/assets/page_all.js
@@ -13,12 +13,41 @@ $(document).ready(function () {
         slidesToScroll: 1,
         asNavFor: '.slider-for',
         dots: false,
-        focusOnSelect: true
+        focusOnSelect: true,
+        centerPadding: '20px',
+        responsive: [
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          }
+         
+        ]
       });
 
       $('.slide-team').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          }
+         
+        ]
       });
+});
+$(function(){
+  $('.menu-trigger').on('click', function() {
+    $(this).toggleClass('active');
+    $(".header-top").toggleClass('active');
+    return false;
+    
+  });
 });
